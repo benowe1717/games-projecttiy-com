12950bfcc8bfc5e4c759c8a3eded80222e17ea50
Benjamin Owen <benjamin@projecttiy.com>
2024-08-27 11:34:07 -0400

    added a composer package for adding in data fixtures, updated playerscontroller to pull players from the database on the /players route

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -2,27 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Player;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PlayersController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
     private string $title = 'Games - Players';
     private array $players = array();
     private int $active_player = -1;
     private array $previous_attempts = array();
 
-    public function __construct()
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->players[] = array('id' => 1, 'name' => 'specter2426', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'ApakefHC', 'play_time' => 6, 'adventure_level' => 25);
-        $this->players[] = array('id' => 2, 'name' => 'plantmafia', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'Barrett Jr', 'play_time' => 50, 'adventure_level' => 25);
-        $this->players[] = array('id' => 3, 'name' => 'BearsFruit', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'Mr Silverado', 'play_time' => 2, 'adventure_level' => 10);
+        $this->entityManager = $entityManager;
+
+        $this->old_players[] = array('id' => 1, 'name' => 'specter2426', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'ApakefHC', 'play_time' => 6, 'adventure_level' => 25);
+        $this->old_players[] = array('id' => 2, 'name' => 'plantmafia', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'Barrett Jr', 'play_time' => 50, 'adventure_level' => 25);
+        $this->old_players[] = array('id' => 3, 'name' => 'BearsFruit', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'Mr Silverado', 'play_time' => 2, 'adventure_level' => 10);
+    }
+
+    private function getPlayers(): void
+    {
+        $playerRepository = $this->entityManager
+            ->getRepository(Player::class);
+        $this->players = $playerRepository->findAll();
     }
 
     #[Route('/players', name: 'app_players')]
     public function index(): Response
     {
+        $this->getPlayers();
+
         return $this->render(
             'players/index.html.twig',
             [
@@ -50,7 +65,7 @@ class PlayersController extends AbstractController
             'players/player/index.html.twig',
             [
                 'title' => $this->title,
-                'players' => $this->players,
+                'players' => $this->old_players,
                 'previous_attempts' => $this->previous_attempts,
                 'active_player' => $id,
                 'player_id' => $id,
@@ -76,7 +91,7 @@ class PlayersController extends AbstractController
             'players/previous/index.html.twig',
             [
                 'title' => $this->title,
-                'players' => $this->players,
+                'players' => $this->old_players,
                 'previous_attempts' => $this->previous_attempts,
                 'active_player' => $id,
                 'player_id' => $id,
