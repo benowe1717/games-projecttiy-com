8c0d73c8395184a1523b261cc0fde0c1df44567e
Benjamin Owen <benjamin@projecttiy.com>
2024-08-27 17:18:33 -0400

    updated PlayerController with docblocks, updated twig templates to use new title format

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * Controller for /players route
+ *
+ * PHP version 8.3
+ *
+ * @category  Controller
+ * @package   Games-projecttiy-com
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://mit-license.org/ MIT
+ * @version   CVS: $Id:$
+ * @link      https://github.com/benowe1717/games-projecttiy-com
+ **/
+
 namespace App\Controller;
 
 use App\Entity\Player;
@@ -8,41 +22,62 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * Controller for /players route
+ *
+ * PHP version 8.3
+ *
+ * @category  Controller
+ * @package   Games-projecttiy-com
+ * @author    Benjamin Owen <benjamin@projecttiy.com>
+ * @copyright 2024 Benjamin Owen
+ * @license   https://mit-license.org/ MIT
+ * @version   Release: 0.0.1
+ * @link      https://github.com/benowe1717/games-projecttiy-com
+ **/
 class PlayersController extends AbstractController
 {
-    private EntityManagerInterface $entityManager;
+    private EntityManagerInterface $_entityManager;
 
-    private string $title = 'Games - Players';
-    private array $players = array();
+    private array $_players = array();
     private int $active_player = -1;
     private array $previous_attempts = array();
 
     public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->entityManager = $entityManager;
+        $this->_entityManager = $entityManager;
 
         $this->old_players[] = array('id' => 1, 'name' => 'specter2426', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'ApakefHC', 'play_time' => 6, 'adventure_level' => 25);
         $this->old_players[] = array('id' => 2, 'name' => 'plantmafia', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'Barrett Jr', 'play_time' => 50, 'adventure_level' => 25);
         $this->old_players[] = array('id' => 3, 'name' => 'BearsFruit', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'Mr Silverado', 'play_time' => 2, 'adventure_level' => 10);
     }
 
-    private function getPlayers(): void
+    /**
+     * Retrieve all players from database
+     *
+     * @return void
+     **/
+    private function _getPlayers(): void
     {
-        $playerRepository = $this->entityManager
+        $playerRepository = $this->_entityManager
             ->getRepository(Player::class);
-        $this->players = $playerRepository->findAll();
+        $this->_players = $playerRepository->findAll();
     }
 
+    /**
+     * Render /players route
+     *
+     * @return Response
+     **/
     #[Route('/players', name: 'app_players')]
     public function index(): Response
     {
-        $this->getPlayers();
+        $this->_getPlayers();
 
         return $this->render(
             'players/index.html.twig',
             [
-                'title' => $this->title,
-                'players' => $this->players,
+                'players' => $this->_players,
                 'active_player' => $this->active_player
             ]
         );
@@ -52,7 +87,7 @@ class PlayersController extends AbstractController
     public function playerIndex(string $id): Response
     {
         $currentPlayer = array();
-        foreach ($this->players as $player) {
+        foreach ($this->_players as $player) {
             if ($player['id'] == $id) {
                 $currentPlayer = $player;
             }
@@ -64,7 +99,6 @@ class PlayersController extends AbstractController
         return $this->render(
             'players/player/index.html.twig',
             [
-                'title' => $this->title,
                 'players' => $this->old_players,
                 'previous_attempts' => $this->previous_attempts,
                 'active_player' => $id,
@@ -90,7 +124,6 @@ class PlayersController extends AbstractController
         return $this->render(
             'players/previous/index.html.twig',
             [
-                'title' => $this->title,
                 'players' => $this->old_players,
                 'previous_attempts' => $this->previous_attempts,
                 'active_player' => $id,
