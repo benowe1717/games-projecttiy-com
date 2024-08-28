5deb41d635bdd5eeea10ae5dc709cc141a204272
Benjamin Owen <benjamin@projecttiy.com>
2024-08-28 09:30:50 -0400

    removed all previous objects as i did not like their relationships, added player object back

---
+++ migrations/Version20240828132748.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240828132748 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE player_milestone DROP FOREIGN KEY FK_EFA0B6BB99E6F5DF');
+        $this->addSql('ALTER TABLE player_milestone DROP FOREIGN KEY FK_EFA0B6BB4B3E2EDA');
+        $this->addSql('DROP TABLE player_milestone');
+        $this->addSql('DROP TABLE milestone');
+        $this->addSql('ALTER TABLE player DROP bio, DROP profile, DROP character_name, DROP attempt_number, DROP play_time');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE player_milestone (player_id INT NOT NULL, milestone_id INT NOT NULL, INDEX IDX_EFA0B6BB99E6F5DF (player_id), INDEX IDX_EFA0B6BB4B3E2EDA (milestone_id), PRIMARY KEY(player_id, milestone_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE milestone (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE player_milestone ADD CONSTRAINT FK_EFA0B6BB99E6F5DF FOREIGN KEY (player_id) REFERENCES player (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE player_milestone ADD CONSTRAINT FK_EFA0B6BB4B3E2EDA FOREIGN KEY (milestone_id) REFERENCES milestone (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE player ADD bio LONGTEXT NOT NULL, ADD profile VARCHAR(255) DEFAULT NULL, ADD character_name VARCHAR(255) NOT NULL, ADD attempt_number INT NOT NULL, ADD play_time INT NOT NULL');
+    }
+}
