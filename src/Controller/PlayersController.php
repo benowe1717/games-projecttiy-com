0e650933e26383f3b03e7c377258d36b6731d49b
Benjamin Owen <benjamin@projecttiy.com>
2024-08-28 20:54:26 -0400

    updated Player entity to have a profilePicture, updated template and web design for Players landing page

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Controller for /players route
+ * Symfony Controller for /players Route
  *
  * PHP version 8.3
  *
@@ -23,9 +23,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 /**
- * Controller for /players route
- *
- * PHP version 8.3
+ * Symfony Controller for /players Route
  *
  * @category  Controller
  * @package   Games-projecttiy-com
@@ -37,28 +35,29 @@ use Symfony\Component\Routing\Attribute\Route;
  **/
 class PlayersController extends AbstractController
 {
-    public EntityManagerInterface $entityManager;
+    private EntityManagerInterface $entityManager;
 
+    public string $title = 'Players';
     public array $players = array();
-    public Player $player;
     public int $activePlayer = -1;
 
+    /**
+     * HomeController constructor
+     *
+     * @param EntityManagerInterface $entityManager Entity Manager
+     **/
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
         $this->getPlayers();
-
-        $this->old_players[] = array('id' => 1, 'name' => 'specter2426', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'ApakefHC', 'play_time' => 6, 'adventure_level' => 25);
-        $this->old_players[] = array('id' => 2, 'name' => 'plantmafia', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'Barrett Jr', 'play_time' => 50, 'adventure_level' => 25);
-        $this->old_players[] = array('id' => 3, 'name' => 'BearsFruit', 'bio' => 'A cool short description of your character.', 'attempt_number' => 1, 'character_name' => 'Mr Silverado', 'play_time' => 2, 'adventure_level' => 10);
     }
 
     /**
-     * Retrieve all players from database
+     * Get all players from the database
      *
      * @return void
      **/
-    public function getPlayers(): void
+    private function getPlayers(): void
     {
         $playerRepository = $this->entityManager
             ->getRepository(Player::class);
@@ -66,20 +65,7 @@ class PlayersController extends AbstractController
     }
 
     /**
-     * Retrieve player from database by ID
-     *
-     * @param string $playerId Player ID
-     *
-     * @return void
-     **/
-    public function getPlayer(string $playerId): void
-    {
-        $playerRepository = $this->entityManager->getRepository(Player::class);
-        $this->player = $playerRepository->find($playerId);
-    }
-
-    /**
-     * Render /players route
+     * / app_home Route
      *
      * @return Response
      **/
@@ -89,56 +75,50 @@ class PlayersController extends AbstractController
         return $this->render(
             'players/index.html.twig',
             [
+                'title' => $this->title,
                 'players' => $this->players,
-                'active_player' => $this->activePlayer
+                'active_player' => $this->activePlayer,
             ]
         );
     }
 
-    /**
-     * Render /players/{id} route
-     *
-     * @param string $playerId Player ID
-     *
-     * @return Response
-     **/
-    #[Route('/players/{playerId}', name: 'app_player')]
-    public function playerIndex(string $playerId): Response
-    {
-        $this->getPlayer($playerId);
-
-        return $this->render(
-            'players/player/index.html.twig',
-            [
-                'players' => $this->players,
-                'active_player' => $playerId,
-                'player' => $this->player,
-            ]
-        );
-    }
-
-    #[Route('/players/{id}/previous/{num}', name: 'app_player_previous')]
-    public function previousIndex(string $id, string $num): Response
-    {
-        $currentPlayer = array();
-        foreach ($this->players as $player) {
-            if ($player['id'] == $id) {
-                $currentPlayer = $player;
-            }
-        }
-
-        $player_id = $id;
-        $this->previous_attempts[] = array('id' => 1, 'number' => 1);
-
-        return $this->render(
-            'players/previous/index.html.twig',
-            [
-                'players' => $this->old_players,
-                'previous_attempts' => $this->previous_attempts,
-                'active_player' => $id,
-                'player_id' => $id,
-                'player' => $currentPlayer
-            ]
-        );
-    }
+    // #[Route('/players/{playerId}', name: 'app_player')]
+    // public function playerIndex(string $playerId): Response
+    // {
+    //     $this->getPlayer($playerId);
+    //
+    //     return $this->render(
+    //         'players/player/index.html.twig',
+    //         [
+    //             'players' => $this->players,
+    //             'active_player' => $playerId,
+    //             'player' => $this->player,
+    //         ]
+    //     );
+    // }
+    //
+    // #[Route('/players/{id}/previous/{num}', name: 'app_player_previous')]
+    // public function previousIndex(string $id, string $num): Response
+    // {
+    //     $currentPlayer = array();
+    //     foreach ($this->players as $player) {
+    //         if ($player['id'] == $id) {
+    //             $currentPlayer = $player;
+    //         }
+    //     }
+    //
+    //     $player_id = $id;
+    //     $this->previous_attempts[] = array('id' => 1, 'number' => 1);
+    //
+    //     return $this->render(
+    //         'players/previous/index.html.twig',
+    //         [
+    //             'players' => $this->old_players,
+    //             'previous_attempts' => $this->previous_attempts,
+    //             'active_player' => $id,
+    //             'player_id' => $id,
+    //             'player' => $currentPlayer
+    //         ]
+    //     );
+    // }
 }
